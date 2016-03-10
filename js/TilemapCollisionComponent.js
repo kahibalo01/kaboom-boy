b62934b93ca8b6cbffbaf30cc7ee4243e10a2f44
Ace <kahibalo01@ggmail.com>
2016-03-10 16:56:40 +0800

    TilemapCollisionComponent update

--- js/TilemapCollisionComponent.js
+++ js/TilemapCollisionComponent.js
@@ -3,7 +3,12 @@
 	// corner values for sliding
 	var NO_CORNER = -2, NO_VALUE = -1;
 
-	function detectTileCollisions(tilemap, passlevel, mEntityCoord, pEntityCoord, mEntitySize, pEntitySize, mTileSize, pTileSize, vel, horizontal){
+	/**
+	 *
+	 *
+	 *
+	 */
+	function detectTileCollisions(tilemap, newVector, passLevel, mEntityCoord, pEntityCoord, mEntitySize, pEntitySize, mTileSize, pTileSize, xaxis){
 		// dir: normalized direction vector
 		// sFront: major axis coordinate of the side facing the direction of motion
 		// mDist: distance moved, with the origin at the nearest side of the next tile
@@ -13,7 +18,13 @@
 		// [pStart, pEnd]: range of the tiles to be checked in the perpendicular axis
 		var pStart, pEnd;
 
+		// tilesTopass: number of tiles along the major axis passed by the entity's path of movement
 		var tilesToPass, curTile, tile, resPos, adVel, corner, i, j;
+
+		// resPos: if the entity collides with a tile, it is the major axis coordinate of the collision
+		var resPos;
+
+		vel = (xaxis ? newVector.dx : newVector.dy);
 
 		if(vel > 0){
 			dir = 1;
@@ -35,18 +46,23 @@
 
 		// Detect collisions
 		resPos = null;
+		adVel = null;
 		curTile = mStart;
+
 		for(i = 0; i < tilesToPass + 1; i++){
 			for(j = pStart; j < pEnd + 1; j++){
-				if(horizontal){
+				if(xaxis === true){
 					tile = tilemap.getTile(j, curTile);
 				}else{
 					tile = tilemap.getTile(curTile, j);
 				}
 
 				// Check if tile is passable
-				if(tile !== null && tile.type > passlevel){
-					resPos = resPos || (curTile * mTileSize) + eOffset;
+				if(tile !== null && tile.type > passLevel){
+					if(resPos === null){
+						resPos = (curTile * mTileSize) + eOffset;
+						adVel = resPos - sFront;
+					}
 				}
 
 			}
@@ -54,32 +70,51 @@
 			if(resPos !== null){
 				break;
 			}
+
 			curTile += dir;
 		}
 
+		
 		if(resPos !== null){
-			return (resPos - sFront);
+			if(xaxis === true){
+				newVector.dx = adVel;
+			}else{
+				newVector.dy = adVel;
+			}
+
+			return true;
 		}else{
-			return vel;
+			return false;
 		}
 	}
 
+
+
 	function TilemapCollisionComponent(tilemap){
 		this._tilemap = tilemap;
+
+		this._newVector = {
+			dx: 0,
+			dy: 0
+		};
 
 		(function(that){
 			var td = tilemap.getTileDimensions();
 			that._tileWidth = td.w;
 			that._tileHeight = td.h;
+
+			that._getTileXY = function(x, y){
+				return tilemap.getTile(y, x);
+			}
+
+			that._getTileYX = function(y, x){
+				return tilemap.getTile(y, x);
+			}
 		})(this);
 
-		//this._tileDimensions = tilemap.getTileDimensions();
-
-		this._getTileXY;
-		this._getTileYX;
 	}
 
-	TilemapCollisionComponent.prototype.moveEntity = function(entity){
+	TilemapCollisionComponent.prototype.moveEntity = function(e){
 		/*
 		 *	This function checks for entity-tilemap collisions and adjusts and corrects them accordingly.
 		 *	The entity is assumed to move only in a single axis (either x-axis or y-axis, but not both).
@@ -89,23 +124,26 @@
 		 *
 		 *
 		 */
+		 this._newVector.dx = e.dx;
+		 this._newVector.dy = e.dy;
 
-
-
-		 if(entity.dx !== 0){
-		 	entity.dx = detectTileCollisions(this._tilemap, entity.passLevel, entity.x, entity.y, entity.w, entity.h, this._tileWidth, 
-		 		this._tileHeight, entity.dx, true);
+		 if(e.dx !== 0){
+		 	if(detectTileCollisions(this._tilemap, this._newVector, e.passLevel, e.x, e.y, e.w, e.h, this._tileWidth, this._tileHeight, true) === true){
+		 		e.dx = this._newVector.dx;
+		 	}
+		 	
 		 }else
-		 if(entity.dy !== 0){
-		 	entity.dy = detectTileCollisions(this._tilemap, entity.passLevel, entity.y, entity.x, entity.h, entity.w, this._tileHeight, 
-		 		this._tileWidth, entity.dy, false);
+		 if(e.dy !== 0){
+		 	if(detectTileCollisions(this._tilemap, this._newVector, e.passLevel, e.y, e.x, e.h, e.w, this._tileHeight, this._tileWidth, false) === true){
+		 		e.dy = this._newVector.dy;
+		 	}
 		 }else{
 		 	// No motion
 		 	return false;
 		 }
 
-		 entity.x += entity.dx;
-		 entity.y += entity.dy;
+		 e.x += e.dx;
+		 e.y += e.dy;
 
 		 return;
 	}
