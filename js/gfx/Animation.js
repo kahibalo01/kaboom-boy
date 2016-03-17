3f64ae5454a4ac8577eb80934eddfd470d1a87a3
Ace <kahibalo01@ggmail.com>
2016-03-17 15:21:31 +0800

    Sprite and Animation changes: now has offsets

--- js/gfx/Animation.js
+++ js/gfx/Animation.js
@@ -1,61 +1,100 @@
 (function(SpriteSheet){
 
-
-	function Animation(spritesheet, numFrames, delay){
+	function Animation(spritesheet, baseX, baseY, numFrames, delay){
 		this._spritesheet = spritesheet;
+		this._baseX = baseX;
+		this._baseY = baseY;
 
 		this._spriteWidth = spritesheet.getSpriteWidth();
 		this._spriteHeight = spritesheet.getSpriteHeight();
 
-		this._numSequence = numFrames.length;
+		this._numSequences = numFrames.length;
 		this._sequences = [];
-		this._currentSequenceIndex;
-		this._currentSequence;
+		this._currentSeqIndex = -1;
+		this._currentSequence = null;
 
-		for(var i = 0; i < numFrames.length; i++){
+		this._playOnce = false;
+		this._isFinished = false;
+		this._paused = false;
+
+		var i = 0;
+
+
+		for(i = 0; i < numFrames.length; i++){
 			this._sequences[i] = {
 				numFrames: numFrames[i],
 				delay: delay[i]
-			};
+			}
 		}
 
 		this._delayCtr = 0;
 		this._curFrame = 0;
-		this._sequenceY = 0;
 
-		this.setSequence(0);
+		this.startSequence(0);
 	}
 
 
-	Animation.prototype.setSequence = function(i){
-		if(i !== this._currentSequenceIndex && i > -1 && i < this._numSequence){
-			this._currentSequenceIndex = i;
-			this._delayCtr = 0;
-			this._curFrame = 0;
-			this._sequenceY = this._spriteHeight * i;
-			this._currentSequence = this._sequences[i];
+	Animation.prototype.startSequence = function(seqIndex, playOnce, canRestart){
+		canRestart = canRestart || false;
+
+		if(seqIndex === this._currentSeqIndex && !canRestart){
+			return false;
 		}
+
+		if(seqIndex > -1 && seqIndex < this._numSequences){
+			this._currentSeqIndex = seqIndex;
+			this._currentSequence = this._sequences[seqIndex];
+			this._delayCtr = this._currentSequence.delay;
+			this._curFrame = 0;
+			this._playOnce = playOnce;
+			this._isFinished = false;
+			return true;
+		}
+
+		return false;
+	}
+
+
+	Animation.prototype.pause = function(){
+		this._paused = true;
+	}
+
+
+	Animation.prototype.resume = function(){
+		this._paused = false;
+	}
+
+
+	Animation.prototype.isFinished = function(){
+		return this._isFinished;
 	}
 
 
 	Animation.prototype.update = function(){
-		if(this._delayCtr === 0){
+		if(this._paused || this._isFinished){
+			return;
+		}
+
+		this._delayCtr--;
+		if(this._delayCtr < 0){
 			this._delayCtr = this._currentSequence.delay;
+
 			this._curFrame++;
-
-			if(this._curFrame > this._currentSequence.numFrames - 1){
+			if(this._curFrame === this._currentSequence.numFrames){
 				this._curFrame = 0;
-			}
 
-		}else{
-			this._delayCtr--;
+				if(this._playOnce){
+					this._isFinished = true;
+				}
+			}
 		}
 	}
 
 
 	Animation.prototype.render = function(ctx, x, y){
-		this._spritesheet.drawSpriteToContext(ctx, this._currentSequenceIndex, this._curFrame, x, y);
+		this._spritesheet.drawSpriteToContext(ctx, this._baseY + this._currentSeqIndex, this._curFrame, x, y);
 	}
+
 
 	window.KaboomBoy.Animation = Animation;
 
--- js/gfx/Spritesheet.js
+++ js/gfx/Spritesheet.js
@@ -1,60 +1,55 @@
 (function(){
 
-	function Spritesheet(image, spriteWidth, spriteHeight){
+	function Spritesheet(image, xBase, yBase, numRows, numCols, spriteWidth, spriteHeight){
 		this._image = image;
-		this._width = image.width;
-		this._height = image.height;
 
+		// Offset on image
+		this._xBase = xBase;
+		this._yBase = yBase;
+
+		// Spritesheet dimensions
+		this._numRows = numRows;
+		this._numCols = numCols;
 		this._spriteWidth = spriteWidth;
 		this._spriteHeight = spriteHeight;
-
-		this._cols = Math.floor(image.width / spriteWidth);
-		this._rows = Math.floor(image.height / spriteHeight);
 	}
+
 
 	Spritesheet.prototype.getSpriteWidth = function(){
 		return this._spriteWidth;
 	}
 
+
 	Spritesheet.prototype.getSpriteHeight = function(){
 		return this._spriteHeight;
 	}
 
-	Spritesheet.prototype.getCols = function(){
-		return this._cols;
-	}
 
 	Spritesheet.prototype.getRows = function(){
-		return this._rows;
+		return this._numRows;
 	}
 
-	Spritesheet.prototype.getDimensions = function(){
-		return {
-			sheetWidth: this._width,
-			sheetHeight: this._height,
-			spriteWidth: this._spriteWidth,
-			spriteHeight: this._spriteHeight
-		};
+
+	Spritesheet.prototype.getCols = function(){
+		return this._numCols;
 	}
+
 
 	Spritesheet.prototype.drawSpriteToContext = function(ctx, row, col, x, y){
-		var sw, sh;
+		var sw = null, sh = null;
 
-		if(row > -1 && row < this._spriteHeight && col > -1 && col < this._spriteWidth){
+		if(row > -1 && row < this._numRows && col > -1 && col < this._numCols){
 			sw = this._spriteWidth;
 			sh = this._spriteHeight;
-			ctx.drawImage(this._image, (col * sw), (row * sh), sw, sh, x, y, sw, sh);
-		}		
+			ctx.drawImage(this._image, this._xBase + (col * sw), this._yBase + (row * sh), sw, sh, x, y, sw, sh);
+		}
 	}
 
-	
+
 	Spritesheet.prototype.drawSpriteOnIndexToContext = function(ctx, index, x, y){
-		var sw = this._spriteWidth, sh = this._spriteHeight;
-		var xSprite = (index % this._cols) * this._spriteWidth;
-		var ySprite = Math.floor(index / this._cols) * this._spriteHeight;
-
-		ctx.drawImage(this._image, xSprite, ySprite, sw, sh, x, y, sw, sh);
+		this.drawSpriteToContext(ctx, Math.floor(index / this._numCols), (index % this._numCols), x, y);
 	}
+
 
 	window.KaboomBoy.Spritesheet = Spritesheet;
 
