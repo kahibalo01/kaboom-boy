4ce87ba6d367ec17d11275ad00875a3896383155
Ace <kahibalo01@ggmail.com>
2016-03-19 13:56:16 +0800

    Tile redesign

--- js/map/Tile.js
+++ js/map/Tile.js
@@ -1,13 +1,33 @@
 (function(){
 
-	/**
-	 *	The basic unit of a tilemap
-	 */
-	function Tile(index, type){
-		this.index = index;
-		this.type = type;
+	function Tile(life, resistance, destructionDelay){
+		this.life = life;
+		this.resistance = resistance;
+
+		this.destructionDelay = destructionDelay;
+		this.destructionCountdown = 0;
 	}
 
-	window.KaboomBoy.Tile = Tile;
+
+	Tile.prototype.explodeTile = function(){
+		this.destructionCountdown = this.destructionDelay;
+	}
+
+
+	Tile.prototype.update = function(){
+		if(this.destructionCountdown > 0 && this.life > 0){
+			this.destructionCountdown--;
+
+			if(this.destructionCountdown === 0){
+				this.life--;
+			}
+		}
+	}
+
+
+	Tile.prototype.isDestroyed = function(){
+		return (this.life === 0);
+	}
+
 
 })();
