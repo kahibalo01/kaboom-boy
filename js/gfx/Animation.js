2657b50371deab4fc244eeba59ce82ae9ab13b66
Ace <kahibalo01@ggmail.com>
2016-03-20 13:24:34 +0800

    Bugfix: Animation.nextFrame previously doesn't move the animation to the next frame, now fixed. Setting sequence without animating and moving frames manually now working as intended.

--- js/gfx/Animation.js
+++ js/gfx/Animation.js
@@ -2,61 +2,91 @@
 
 	var INDEFINITE = -1;
 
-	function Animation(spritesheet, baseX, baseY, numFrames, delay){
+	/**
+	 * Animation manager
+	 */
+	function Animation(spritesheet, animationData){
 		this._spritesheet = spritesheet;
-		this._baseX = baseX;
-		this._baseY = baseY;
 
-		this._spriteWidth = spritesheet.getSpriteWidth();
-		this._spriteHeight = spritesheet.getSpriteHeight();
+		// Add code to check for undefined and invalid values in animationData
 
-		this._numSequences = numFrames.length;
+		this._colBase = animationData.colBase || 0;
+		this._rowBase = animationData.rowBase || 0;
+
+		this._numSequences = animationData.numFrames.length;
 		this._sequences = [];
 
-		var i = 0;
+		var i = 0,
+			numFrames = animationData.numFrames,
+			speed = animationData.speed;
 
 		for(i = 0; i < numFrames.length; i++){
 			this._sequences[i] = {
 				numFrames: numFrames[i],
-				delay: delay[i]
+				speed: speed[i]
 			}
 		}
 	}
 
+	
 	Animation.prototype.createHandle = function(){
 		return new AnimationHandle(this);
 	}
 
 
+	/**
+	 * A handle for an Animation data object.
+	 * Supports:
+	 *		- playing an animation sequence a definite/indefinite number of times
+	 *		- pausing/unpausing an animation
+	 *		- manual advance to next frame
+	 */
 	function AnimationHandle(animation){
 		this._animation = animation;
 
-		this._currentSequenceIndex = -1;
-		this._currentSequence;
+		this.currentSequenceIndex = 0;
+		this.currentFrame = 0;
 
-		this._delayCtr = 0;
-		this._curFrame = 0;
+		this._currentSequence = null;
+		this._finishCallback = null;
 
+		this._delay = 0;
 		this._timesToPlay = 0;
-		this._paused = false;
 	}
+
+
 
 	AnimationHandle.prototype.INDEFINITE = INDEFINITE;
 
-	AnimationHandle.prototype.setSequence = function(seqIndex, timesToPlay){
-		if(seqIndex === this._currentSequenceIndex){
-			return false;
-		}
+	/**
+	 *	Sets the animation sequence to the provided `seqIndex`. Note that calling this method
+	 *	will turn back the current frame to 0.
+	 *
+	 */
+	AnimationHandle.prototype.setSequence = function(seqIndex, timesToPlay, callback){
 
 		if(seqIndex > -1 && seqIndex < this._animation._numSequences){
-			this._currentSequenceIndex = seqIndex;
+			this.currentSequenceIndex = seqIndex;
 			this._currentSequence = this._animation._sequences[seqIndex];
 
-			this._delayCtr = this._currentSequence.delay;
-			this._curFrame = 0;
+			this._delay = this._currentSequence.speed;
+			this.currentFrame = 0;
 
-			this._timesToPlay = timesToPlay || INDEFINITE;
+			this._timesToPlay = (timesToPlay < 0 ? INDEFINITE: timesToPlay);
+
+
+			if(this._timesToPlay > 0){
+				this._finishCallback = callback;
+			}else{
+				this._finishCallback = null;
+			}
+
 		}
+	}
+
+
+	AnimationHandle.prototype.nextFrame = function(){
+		this.currentFrame = (this.currentFrame + 1) % this._currentSequence.numFrames;
 	}
 
 
@@ -65,40 +95,48 @@
 		return this;
 	}
 
+
 	AnimationHandle.prototype.unpause = function(){
 		this._paused = false;
 		return this;
 	}
 
 
-	AnimationHandle.prototype.isFinished = function(){
-		return (this._timesToPlay == 0);
-	}
-
-
+	// Check later, please.
+	// Seems to be working
 	AnimationHandle.prototype.update = function(){
 		if(this._paused || this._timesToPlay === 0){
 			return;
 		}
 
-		this._delayCtr--;
-		if(this._delayCtr < 0){
-			this._delayCtr = this._currentSequence.delay;
+		this._delay--;
+		if(this._delay === 0){
+			this.currentFrame++;
 
-			this._curFrame++;
-			if(this._curFrame === this._currentSequence.numFrames){
-				this._curFrame = 0;
+			if(this.currentFrame === this._currentSequence.numFrames){
+				this.currentFrame = 0;
 
 				if(this._timesToPlay !== INDEFINITE){
 					this._timesToPlay--;
+				
+					if(this._timesToPlay === 0 && this._finishCallback !== null){
+						this._finishCallback();
+						return;
+					}
 				}
+				
 			}
+
+			this._delay = this._currentSequence.speed;
 		}
+
 	}
 
 
+
+
 	AnimationHandle.prototype.render = function(ctx, x, y){
-		this._animation._spritesheet.drawSpriteToContext(ctx, this._animation._baseY + this._currentSequenceIndex, this._curFrame, x, y);
+		this._animation._spritesheet.drawSpriteToContext(ctx, this._animation._rowBase + this.currentSequenceIndex, this._animation._colBase + this.currentFrame, x, y);
 	}
 
 
